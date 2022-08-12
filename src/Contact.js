882cbd831f84071a4883b5b9d9df3ea7211ca5d8
Varun Kansal <varunkansal2111@gmail.com>
2022-08-13 00:11:15 +0530

    finally

--- src/Contact.js
+++ src/Contact.js
@@ -2,8 +2,23 @@ import React from 'react'
 import Navbar from './Navbar'
 import Bottom from './Bottom'
 import './Contact.css'
+import emailjs from 'emailjs-com'
+
 
 function Contact() {
+    function sentEmail(e){
+        e.preventDefault();
+
+        emailjs.sendForm("service_e9yp91d","template_0rz5nbu",e.target,"PdxBDs8miZAx0YrOH").then(res=>{
+            alert("Submitted");
+            window.location.reload(false);
+        }).catch(err=>{
+            alert("Internal Server Error");
+            window.location.reload(false);
+        }
+        
+        );
+    }
   return (
     <>
         <Navbar page="contact"/>
@@ -16,10 +31,10 @@ function Contact() {
                     Looking forward to answering your email
                 </p>
                 <hr className="line" />
-                <form >
+                <form onSubmit={sentEmail}>
                     <input className="support_input" name="name"  placeholder="Name" type="text" />
                     <input className="support_input" name="mobileno"  placeholder="Mobile Number" type="text" />
-                    <input className="support_input" name="email"  placeholder="Email" type="text" />
+                    <input className="support_input" name="user_email"  placeholder="Email" type="text" />
                     <textarea className="support_input" name="message"  style={{ "resize": "vertical", "height": "100px" }} placeholder="Message" type="text"></textarea>
                     <div style={{ "width": "100%", "textAlign": "center" }}>
                         <button id="submit_form" type="submit">
